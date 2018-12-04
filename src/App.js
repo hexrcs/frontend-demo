8354598b73f872f482b45a55b6e65bd1f8a110a7
Xiaoru Li <dev@xiaoru.li>
2018-12-04 23:21:20 +0100

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hi
+        <Button color="primary" variant="contained" />
       </div>
     );
   }
