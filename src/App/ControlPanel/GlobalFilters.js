c243d6d95310cf5c74640f25a310eb3540cef5e9
Xiaoru Li <dev@xiaoru.li>
2018-12-10 17:29:31 +0100

    Containers for Global Filters and Notifications

--- src/App/ControlPanel/GlobalFilters.js
+++ src/App/ControlPanel/GlobalFilters.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
-import { Container } from "@components";
+import styled from "@emotion/styled";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+const Container = styled.div`
+  flex: 1;
+  align-self: flex-start;
+  margin: 0 1rem;
+`;
+
+const PaperContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  height: 10rem;
+`;
 
 export default class GlobalFilters extends Component {
   render() {
-    return <div>This is Global Filters</div>;
+    return (
+      <Container>
+        <Paper elevation={1}>
+          <PaperContent>
+            <Typography variant="subheading" component="h3">
+              Global Filters
+            </Typography>
+          </PaperContent>
+        </Paper>
+      </Container>
+    );
   }
 }
--- src/App/ControlPanel/Notifications.js
+++ src/App/ControlPanel/Notifications.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
-import { Container } from "@components";
+import styled from "@emotion/styled";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+const Container = styled.div`
+  flex: 1;
+  align-self: flex-start;
+  margin: 0 1rem;
+`;
+
+const PaperContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  height: 10rem;
+`;
 
 export default class Notifications extends Component {
   render() {
-    return <div>This is Notifications</div>;
+    return (
+      <Container>
+        <Paper elevation={1}>
+          <PaperContent>
+            <Typography variant="subheading" component="h3">
+              Notifications
+            </Typography>
+          </PaperContent>
+        </Paper>
+      </Container>
+    );
   }
 }
