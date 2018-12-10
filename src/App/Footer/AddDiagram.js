499a20bbcf7113011b4528aa1dd5f870433c8bc2
Xiaoru Li <dev@xiaoru.li>
2018-12-10 21:44:41 +0100

    Implement footer section

--- src/App/Footer/AddDiagram.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class AddDiagram extends Component {
-  render() {
-    return <div>Add diagram button</div>;
-  }
-}
--- src/App/Footer/index.js
+++ src/App/Footer/index.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
+import styled from "@emotion/styled";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import AddBoxIcon from "@material-ui/icons/AddBox";
 
-import AddDiagram from "./AddDiagram";
+const Container = styled.div`
+  display: flex;
+  margin: 0 1rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledAddBoxIcon = styled(AddBoxIcon)`
+  margin-right: 0.5rem;
+`;
 
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        <AddDiagram />
-      </div>
+      <Container>
+        <Typography variant="body1" color="textSecondary">
+          {"© 2018 ADIN Frontend Contributors"}
+        </Typography>
+        <Button variant="contained" color="primary">
+          <StyledAddBoxIcon />
+          Add Diagram
+        </Button>
+      </Container>
     );
   }
 }
